a29a95a27c2e2ad76f5c75d3f7fdcd3154fc5515
Yazan Hashoum <hyyazan234@gmail.com>
2024-05-14 22:37:24 +0200

    connected the UI representation to the actual AlarmCollection; Flexible Grid; Add funcionality

--- AHIFusion/AHIFusion/View/Clock/AlarmContent.xaml.cs
+++ AHIFusion/AHIFusion/View/Clock/AlarmContent.xaml.cs
@@ -2,20 +2,222 @@ using AHIFusion.Model;
 using Windows.Graphics.Capture;
 
 namespace AHIFusion;
+
+using System.Collections.Specialized;
 using AHIFusion.Model;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media.Animation;
+using Uno;
+
 /// <summary>
 /// An empty page that can be used on its own or navigated to within a Frame.
 /// </summary>
 public partial class AlarmContent : Page
 {
-    public Alarm alarm1 { get; set; }
+    public Alarm alarm1 { get; set; } = new Alarm("Joudi", new TimeOnly(8, 10), true);
+    public Alarm alarm2 { get; set; } = new Alarm("Alarm 2", new TimeOnly(5, 45), false);
+    public Alarm alarm3 { get; set; } = new Alarm("Test", new TimeOnly(12, 30), true);
+    public Alarm alarm4 { get; set; } = new Alarm("Langer Text für Hausaufgaben", new TimeOnly(22, 53), false);
+    public Alarm alarm5 { get; set; } = new Alarm("Langer Text für Hausaufgaben", new TimeOnly(22, 53), false);
 
     public AlarmContent()
     {
         this.InitializeComponent();
 
-        alarm1 = new Alarm("Joudi", new TimeOnly(8, 0), false);
+        AlarmCollection.Alarms.Add(alarm1);
+        AlarmCollection.Alarms.Add(alarm2);
+        AlarmCollection.Alarms.Add(alarm3);
+        AlarmCollection.Alarms.Add(alarm4);
+        AlarmCollection.Alarms.Add(alarm5);
 
         DataContext = this;
+
+        InitializeControls();
+
+        AlarmCollection.Alarms.CollectionChanged += Alarms_CollectionChanged;
+
+        
+    }
+
+    private void InitializeControls()
+    {
+        MainGrid.Children.Clear();
+        MainGrid.RowDefinitions.Clear();
+
+        for (int i = 0; i < AlarmCollection.Alarms.Count; i++)
+        {
+            AlarmControl alarmControl = new AlarmControl
+            {
+                DataContext = AlarmCollection.Alarms[i],
+                Margin = new Thickness(10),
+                MaxHeight = 320
+            };
+
+            Binding timeBinding = new Binding
+            {
+                Path = new PropertyPath("Time"),
+                Mode = BindingMode.TwoWay
+            };
+
+            Binding titleBinding = new Binding
+            {
+                Path = new PropertyPath("Title"),
+                Mode = BindingMode.TwoWay
+            };
+
+            Binding isOnBinding = new Binding
+            {
+                Path = new PropertyPath("IsOn"),
+                Mode = BindingMode.TwoWay
+            };
+
+            alarmControl.SetBinding(AlarmControl.TimeProperty, timeBinding);
+            alarmControl.SetBinding(AlarmControl.TitleProperty, titleBinding);
+            alarmControl.SetBinding(AlarmControl.IsOnProperty, isOnBinding);
+
+            int row = i / 2;
+            int column = i % 2;
+
+            if (row >= MainGrid.RowDefinitions.Count)
+            {
+                MainGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            }
+
+            Grid.SetRow(alarmControl, row);
+            Grid.SetColumn(alarmControl, column);
+
+            MainGrid.Children.Add(alarmControl);
+
+            for (int j = MainGrid.RowDefinitions.Count - 1; j >= 0; j--)
+            {
+                bool rowIsOccupied = MainGrid.Children.Cast<UIElement>().Any(e => Grid.GetRow((FrameworkElement)e) == j);
+
+                if (!rowIsOccupied)
+                {
+                    MainGrid.RowDefinitions.RemoveAt(j);
+                }
+            }
+        }
+
+        AddRectControl addRectControl = new AddRectControl
+        {
+            Margin = new Thickness(10),
+            MaxHeight = 320
+        };
+
+        int addRectRow = AlarmCollection.Alarms.Count / 2;
+        int addRectColumn = AlarmCollection.Alarms.Count % 2;
+
+        if (addRectRow >= MainGrid.RowDefinitions.Count)
+        {
+            MainGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+        }
+
+        Grid.SetRow(addRectControl, addRectRow);
+        Grid.SetColumn(addRectControl, addRectColumn);
+
+        MainGrid.Children.Add(addRectControl);
+    }
+
+    private void Alarms_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+        {
+            foreach (Alarm newAlarm in e.NewItems)
+            {
+                AddAlarmControl(newAlarm);
+            }
+        }
+    }
+
+    private void AddAlarmControl(Alarm alarm)
+    {
+        // diesser Abschnitt löscht das AddRectControl, wenn es existiert (später wirds als letztes Element
+        // zurück hinzugefügt)
+        var addRectControl = MainGrid.Children.OfType<AddRectControl>().FirstOrDefault();
+        if (addRectControl != null)
+        {
+            MainGrid.Children.Remove(addRectControl);
+        }
+
+        // das AlarmControl wird hinzugefügt
+        MainGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+
+        AlarmControl alarmControl = new AlarmControl
+        {
+            DataContext = alarm,
+            Margin = new Thickness(10),
+            MaxHeight = 320
+        };
+
+        Binding timeBinding = new Binding
+        {
+            Path = new PropertyPath("Time"),
+            Mode = BindingMode.TwoWay
+        };
+
+        Binding titleBinding = new Binding
+        {
+            Path = new PropertyPath("Title"),
+            Mode = BindingMode.TwoWay
+        };
+
+        Binding isOnBinding = new Binding
+        {
+            Path = new PropertyPath("IsOn"),
+            Mode = BindingMode.TwoWay
+        };
+
+        alarmControl.SetBinding(AlarmControl.TimeProperty, timeBinding);
+        alarmControl.SetBinding(AlarmControl.TitleProperty, titleBinding);
+        alarmControl.SetBinding(AlarmControl.IsOnProperty, isOnBinding);
+
+        int i = MainGrid.Children.OfType<AlarmControl>().Count();
+
+        int row = i / 2;
+        int column = i % 2;
+
+        if (row >= MainGrid.RowDefinitions.Count)
+        {
+            MainGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+        }
+
+        Grid.SetRow(alarmControl, row);
+        Grid.SetColumn(alarmControl, column);
+
+        MainGrid.Children.Add(alarmControl);
+
+        // Animation für das Hinzufügen des AlarmControls
+        var animation = new DoubleAnimation
+        {
+            From = 0,
+            To = 1,
+            Duration = new Duration(TimeSpan.FromSeconds(0.5))
+        };
+
+        var storyboard = new Storyboard();
+        Storyboard.SetTarget(animation, alarmControl);
+        Storyboard.SetTargetProperty(animation, "Opacity");
+        storyboard.Children.Add(animation);
+
+        storyboard.Begin();
+
+        // das AddRectControl wird als letztes Element hinzugefügt
+
+        if (addRectControl != null)
+        {
+            int addRectRow = MainGrid.Children.OfType<AlarmControl>().Count() / 2;
+            int addRectColumn = MainGrid.Children.OfType<AlarmControl>().Count() % 2;
+
+            if (addRectRow >= MainGrid.RowDefinitions.Count)
+            {
+                MainGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            }
+
+            Grid.SetRow(addRectControl, addRectRow);
+            Grid.SetColumn(addRectControl, addRectColumn);
+
+            MainGrid.Children.Add(addRectControl);
+        }
     }
 }

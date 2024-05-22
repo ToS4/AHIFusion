4959607c94964343e50ff96e2941934a207708d2
mirko4001 <hyyazan234@gmail.com>
2024-05-22 20:15:50 +0200

    fixed dictionary bug; added deletebutton and functionality

--- AHIFusion/AHIFusion/View/Clock/Alarm/EditAlarm.xaml.cs
+++ AHIFusion/AHIFusion/View/Clock/Alarm/EditAlarm.xaml.cs
@@ -7,7 +7,7 @@ public sealed partial class EditAlarm : ContentDialog
 {
     public string NameEdit { get; set; }
     public TimeSpan TimeEdit { get; set; } = new TimeSpan();
-    public Dictionary<string, bool> DaysEdit { get; set; } = new Dictionary<string, bool>();
+    public Dictionary<string, bool> DaysEdit { get; set; }
 
     public Alarm alarmEdit;
 
@@ -17,13 +17,27 @@ public sealed partial class EditAlarm : ContentDialog
         alarmEdit = alarm;
         NameEdit = alarm.Title;
         TimeEdit = alarm.Time.ToTimeSpan();
-        DaysEdit = alarm.Days;
+        DaysEdit = new Dictionary<string, bool>(alarm.Days);
 	}
 
 	private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
 	{
         alarmEdit.Title = NameEdit;
         alarmEdit.Time = TimeOnly.FromTimeSpan(TimeEdit);
-        alarmEdit.Days = DaysEdit; //Das speichert, obwohl man canceled; es updated die UI nicht
+        alarmEdit.Days = DaysEdit;
+    }
+
+    private void DeleteButton_Click(object sender, RoutedEventArgs e)
+    {
+        for (int i = 0; i < AlarmCollection.Alarms.Count; i++)
+        {
+            if (alarmEdit == AlarmCollection.Alarms[i])
+            {
+                AlarmCollection.Alarms.RemoveAt(i);
+                break;
+            }
+        }
+
+        this.Hide();
     }
 }

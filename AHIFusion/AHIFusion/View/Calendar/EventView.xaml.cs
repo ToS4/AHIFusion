4058ceb71610de4c1b27aa9cb26ba76f263d9c6d
ToS4 <ayo.mohamad@hotmail.com>
2024-06-03 11:14:27 +0200

    added event view files

---
+++ AHIFusion/AHIFusion/View/Calendar/EventView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+namespace AHIFusion.View.Calendar;
+public sealed partial class EventView : ContentDialog
+{
+    public EventView()
+    {
+        this.InitializeComponent();
+    }
+
+    private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+    {
+    }
+
+    private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+    {
+    }
+}

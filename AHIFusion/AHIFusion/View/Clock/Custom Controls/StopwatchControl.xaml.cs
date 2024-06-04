670af1680d7dc5ae0d95423b972cc01d2f10f75b
mirko4001 <hyyazan234@gmail.com>
2024-06-04 21:48:40 +0200

    configured StopwatchControl.cs (buttons and binding)

--- AHIFusion/AHIFusion/View/Clock/Custom Controls/StopwatchControl.xaml.cs
+++ AHIFusion/AHIFusion/View/Clock/Custom Controls/StopwatchControl.xaml.cs
@@ -10,6 +10,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Windows.Devices.Display.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
@@ -18,11 +19,65 @@ using Windows.Foundation.Collections;
 namespace AHIFusion;
 public sealed partial class StopwatchControl : UserControl
 {
+    DispatcherTimer dispatcherTimer = new DispatcherTimer();
+    System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+
     public StopwatchControl()
     {
         this.InitializeComponent();
+        dispatcherTimer.Tick += DispatcherTimer_Tick;
+        dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 1);
+    }
+
+    private void DispatcherTimer_Tick(object? sender, object e)
+    {
+        if (stopwatch.IsRunning)
+        {
+            ElapsedTime = stopwatch.Elapsed;
+        }
     }
 
     public static readonly DependencyProperty StartTimeProperty = DependencyProperty.Register("StartTime", typeof(DateTime), typeof(StopwatchControl), new PropertyMetadata(0));
+    public DateTime StartTime
+    {
+        get { return (DateTime)GetValue(StartTimeProperty); }
+        set { SetValue(StartTimeProperty, value); }
+    }
 
+    public static readonly DependencyProperty ElapsedTimeProperty = DependencyProperty.Register("ElapsedTime", typeof(TimeSpan), typeof(StopwatchControl), new PropertyMetadata(new TimeSpan(0, 0, 0)));
+    public TimeSpan ElapsedTime
+    {
+        get { return (TimeSpan)GetValue(ElapsedTimeProperty); }
+        set { SetValue(ElapsedTimeProperty, value); }
+    }
+
+    public static readonly DependencyProperty IsRunningProperty = DependencyProperty.Register("IsRunning", typeof(bool), typeof(StopwatchControl), new PropertyMetadata(false));
+    public bool IsRunning
+    {
+        get { return (bool)GetValue(IsRunningProperty); }
+        set { SetValue(IsRunningProperty, value); }
+    }
+
+    private void StartStopButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (!stopwatch.IsRunning)
+        {
+            stopwatch.Start();
+            dispatcherTimer.Start();
+            StartStopButton.Content = "Stop";
+        }
+        else
+        {
+            stopwatch.Stop();
+            dispatcherTimer.Stop();
+            StartStopButton.Content = "Start";
+        }
+
+    }
+
+    private void ResetButton_Click(object sender, RoutedEventArgs e)
+    {
+        stopwatch.Reset();
+        ElapsedTime = new TimeSpan(0, 0, 0, 0, 0);
+    }
 }

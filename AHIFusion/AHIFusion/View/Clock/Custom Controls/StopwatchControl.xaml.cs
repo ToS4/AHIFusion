fb7f66b308a96e5976cdc3a2dba09cac8b5d9ba3
mirko4001 <hyyazan234@gmail.com>
2024-06-05 12:40:17 +0200

    stopwatch finished

--- AHIFusion/AHIFusion/View/Clock/Custom Controls/StopwatchControl.xaml.cs
+++ AHIFusion/AHIFusion/View/Clock/Custom Controls/StopwatchControl.xaml.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -10,6 +12,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Uno.Extensions.ValueType;
 using Windows.Devices.Display.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -21,13 +24,64 @@ public sealed partial class StopwatchControl : UserControl
 {
     DispatcherTimer dispatcherTimer = new DispatcherTimer();
     System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+    public ObservableCollection<string> LapList = new ObservableCollection<string>();
 
     public StopwatchControl()
     {
         this.InitializeComponent();
         dispatcherTimer.Tick += DispatcherTimer_Tick;
         dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 1);
+        LapList.CollectionChanged += LapList_CollectionChanged;
         ResetTextBlocks();
+    }
+
+    private void LapList_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        LapListView.Items.Clear();
+        double flagGridHeight = FlagGrid.ActualHeight;
+
+        for (int i = 0; i < LapList.Count; i++)
+        {
+            List<string> parts = LapList[i].Split(";").ToList();
+            // "1", "00:00:20,35", "00:01:53:81"
+
+            string countString = parts[0];
+            string timeString = parts[1];
+            string totalString = parts[2];
+
+            TextBlock countTextBlock = new TextBlock();
+            countTextBlock.Text = countString;
+            countTextBlock.FontSize = 18;
+            countTextBlock.Foreground = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 0, 0, 0));
+            countTextBlock.Margin = new Thickness(22, 0, 0, 0);
+
+            TextBlock timeTextBlock = new TextBlock();
+            timeTextBlock.Text = timeString;
+            timeTextBlock.FontSize = 18;
+            timeTextBlock.Foreground = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 0, 0, 0));
+            timeTextBlock.Margin = new Thickness(22, 0, 0, 0);
+
+            TextBlock totalTextBlock = new TextBlock();
+            totalTextBlock.Text = totalString;
+            totalTextBlock.FontSize = 18;
+            totalTextBlock.Margin = new Thickness(22, 0, 0, 0);
+            totalTextBlock.Foreground = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 0, 0, 0));
+
+            Grid grid = new Grid();
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(0.6, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+
+            grid.Children.Add(countTextBlock);
+            grid.Children.Add(timeTextBlock);
+            grid.Children.Add(totalTextBlock);
+
+            Grid.SetColumn(countTextBlock, 0);
+            Grid.SetColumn(timeTextBlock, 1);
+            Grid.SetColumn(totalTextBlock, 2);
+
+            LapListView.Items.Insert(0, grid);
+        }
     }
 
     private void DispatcherTimer_Tick(object? sender, object e)
@@ -70,12 +124,14 @@ public sealed partial class StopwatchControl : UserControl
         {
             stopwatch.Start();
             dispatcherTimer.Start();
+            IsRunning = true;
             StartStopButton.Content = "Stop";
         }
         else
         {
             stopwatch.Stop();
             dispatcherTimer.Stop();
+            IsRunning = false;
             StartStopButton.Content = "Start";
         }
 
@@ -86,6 +142,7 @@ public sealed partial class StopwatchControl : UserControl
         stopwatch.Reset();
         dispatcherTimer.Stop();
         StartStopButton.Content = "Start";
+        LapList.Clear();
         ElapsedTime = new TimeSpan(0, 0, 0, 0, 0);
         ResetTextBlocks();
     }
@@ -96,5 +153,46 @@ public sealed partial class StopwatchControl : UserControl
         minuteTextBlock.Text = "00";
         secondTextBlock.Text = "00";
         msTextBlock.Text = "00";
+    }
+
+    private void FlagButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (!stopwatch.IsRunning)
+        {
+            return;
+        }
+        TimeSpan flagTime = new TimeSpan(0, 0, 0, 0, 0);
+
+        if (LapList.Count == 0)
+        {
+            flagTime = ElapsedTime;
+        }
+        else
+        {
+            TimeSpan totalFlagTime = new TimeSpan(0, 0, 0, 0, 0);
+            for (int i = 0; i < LapList.Count; i++)
+            {
+                List<string> parts = LapList[i].Split(";").ToList();
+                // "1", "00:00:20,35", "00:01:53:81"
+
+                string format = @"hh\:mm\:ss\,ff";
+                TimeSpan tmp = TimeSpan.ParseExact(parts[1], format, null);
+                totalFlagTime = totalFlagTime.Add(tmp);
+            }
+
+            flagTime = ElapsedTime.Subtract(totalFlagTime);
+        }
+
+        string countString = $"{LapList.Count + 1}";
+        string timeString = $"{flagTime.Hours.ToString("D2")}:" +
+            $"{flagTime.Minutes.ToString("D2")}:" +
+            $"{flagTime.Seconds.ToString("D2")}," +
+            $"{flagTime.Milliseconds.ToString("D3").Substring(0,2)}";
+        string totalString = $"{ElapsedTime.Hours.ToString("D2")}:" +
+            $"{ElapsedTime.Minutes.ToString("D2")}:" +
+            $"{ElapsedTime.Seconds.ToString("D2")}," +
+            $"{ElapsedTime.Milliseconds.ToString("D3").Substring(0, 2)}";
+
+        LapList.Add($"{countString};{timeString};{totalString}");
     }
 }

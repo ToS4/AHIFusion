da62af3e0ddd7b70ef0b2d4430d77d1fc112cf29
ToS4 <ayo.mohamad@hotmail.com>
2024-06-08 12:55:53 +0200

    added weather to homepage

--- AHIFusion/AHIFusion/View/HomePage.xaml.cs
+++ AHIFusion/AHIFusion/View/HomePage.xaml.cs
@@ -1,13 +1,53 @@
 using System.Drawing;
+using System.Text.Json;
+using Microsoft.UI.Xaml.Media.Imaging;
+using Windows.Devices.Geolocation;
 
 namespace AHIFusion
 {
 	public sealed partial class HomePage : Page
 	{
-		public HomePage()
+        private const string OpenWeatherMapApiKey = "e7c7b443582143eb2bdb819309c83af4";
+        private const string OpenWeatherMapApiUrl = "http://api.openweathermap.org/data/2.5/weather";
+
+        public HomePage()
 		{
 			this.InitializeComponent();
-		}
+            LocateAndLoadWeatherData();
+        }
+
+        private async void LocateAndLoadWeatherData()
+        {
+            var geoLocator = new Geolocator();
+            var position = await geoLocator.GetGeopositionAsync();
+            var latitude = position.Coordinate.Point.Position.Latitude;
+            var longitude = position.Coordinate.Point.Position.Longitude;
+
+            await LoadWeatherData(latitude, longitude);
+        }
+
+        private async Task LoadWeatherData(double latitude, double longitude)
+        {
+            string url = $"{OpenWeatherMapApiUrl}?lat={latitude}&lon={longitude}&appid={OpenWeatherMapApiKey}&units=metric";
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetStringAsync(url);
+                var weatherData = JsonDocument.Parse(response);
+
+                var cityName = weatherData.RootElement.GetProperty("name").GetString();
+                var weatherDescription = weatherData.RootElement.GetProperty("weather")[0].GetProperty("description").GetString();
+                var iconCode = weatherData.RootElement.GetProperty("weather")[0].GetProperty("icon").GetString();
+                var temperature = weatherData.RootElement.GetProperty("main").GetProperty("temp").GetDouble();
+
+                CityTextBlock.Text = cityName;
+                WeatherDescriptionTextBlock.Text = weatherDescription;
+                TemperatureTextBlock.Text = $"{temperature}°C";
+
+                string iconUrl = $"http://openweathermap.org/img/wn/{iconCode}.png";
+                WeatherIcon.Source = new BitmapImage(new Uri(iconUrl));
+            }
+        }
 
         private void ReplaceTab(Page page, string header, SymbolIconSource icon)
         {

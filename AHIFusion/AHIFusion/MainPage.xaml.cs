0b07f63fa12811f58c30c0dc8868f8a12c2bf4fa
ToS4 <ayo.mohamad@hotmail.com>
2024-06-08 09:23:46 +0200

    working homepage tab buttons

--- AHIFusion/AHIFusion/MainPage.xaml.cs
+++ AHIFusion/AHIFusion/MainPage.xaml.cs
@@ -6,7 +6,7 @@ namespace AHIFusion;
 
 public sealed partial class MainPage : Page
 {
-    public ObservableCollection<TabViewItem> Tabs { get; set; }
+    public static ObservableCollection<TabViewItem> Tabs { get; set; }
 
     public MainPage()
     {
@@ -41,47 +41,27 @@ public sealed partial class MainPage : Page
         Tabs.CollectionChanged += Tabs_CollectionChanged;
         AddHomeTab();
 
-        var noteTab = AddTab(new NotesPage());
-        noteTab.Header = "Notes";
-        noteTab.IconSource = new SymbolIconSource { Symbol = Symbol.Edit };
-
-        var noteTab1 = AddTab(new NotesPage());
-        noteTab1.Header = "Notes";
-        noteTab1.IconSource = new SymbolIconSource { Symbol = Symbol.Edit };
-
-        var calendarTab1 = AddTab(new CalendarPage());
-        calendarTab1.Header = "Calendar";
-        calendarTab1.IconSource = new SymbolIconSource { Symbol = Symbol.Calendar };
-
-        var clockTab = AddTab(new ClockPage());
-        clockTab.Header = "Clock";
-        clockTab.IconSource = new SymbolIconSource { Symbol = Symbol.Clock };
-
-        var todoTab = AddTab(new TodoPage());
-        todoTab.Header = "Todo";
-        todoTab.IconSource = new SymbolIconSource { Symbol = Symbol.Go};
-
-
         DataContext = this;
     }
 
-    private TabViewItem AddTab(Page page)
+    public static TabViewItem CreateTab(Page page)
     {
         TabViewItem tabViewItem = new TabViewItem()
         {
             IsClosable = false,
             Content = page,
         };
-        Tabs.Add(tabViewItem);
+
 
         return tabViewItem;
     }
 
     private void AddHomeTab()
     {
-        TabViewItem tab = AddTab(new HomePage());
+        TabViewItem tab = CreateTab(new HomePage());
         tab.Header = "Home";
         tab.IconSource = new SymbolIconSource { Symbol = Symbol.Home };
+        Tabs.Add(tab);
     }
 
     private void TabView_AddTabButtonClick(TabView sender, object args)
--- AHIFusion/AHIFusion/View/HomePage.xaml.cs
+++ AHIFusion/AHIFusion/View/HomePage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Drawing;
+
 namespace AHIFusion
 {
 	public sealed partial class HomePage : Page
@@ -6,5 +8,68 @@ namespace AHIFusion
 		{
 			this.InitializeComponent();
 		}
-	}
+
+        private void ReplaceTab(Page page, string header, SymbolIconSource icon)
+        {
+            TabViewItem tab = MainPage.CreateTab(page);
+            tab.Header = header;
+            tab.IconSource = icon;
+            tab.IsSelected = true;
+
+            foreach (TabViewItem tabViewItem in MainPage.Tabs)
+            {
+                if (tabViewItem.Content == this)
+                {
+                    int index = MainPage.Tabs.IndexOf(tabViewItem);
+
+                    if (index != -1)
+                    {
+                        MainPage.Tabs[index] = tab;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Object to replace not found in the list.");
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        private void Todo_Click(object sender, RoutedEventArgs e)
+        {
+            ReplaceTab(
+                new TodoPage(), 
+                "Todo", 
+                new SymbolIconSource { Symbol = Symbol.Go }
+                );
+        }
+
+        private void Calendar_Click(object sender, RoutedEventArgs e)
+        {
+            ReplaceTab(
+                new CalendarPage(),
+                "Calendar",
+                new SymbolIconSource { Symbol = Symbol.Calendar }
+                );
+        }
+
+        private void Notes_Click(object sender, RoutedEventArgs e)
+        {
+            ReplaceTab(
+                new NotesPage(),
+                "Notes",
+                new SymbolIconSource { Symbol = Symbol.Edit }
+                );
+        }
+
+        private void Clock_Click(object sender, RoutedEventArgs e)
+        {
+            ReplaceTab(
+                new ClockPage(),
+                "Clock",
+                new SymbolIconSource { Symbol = Symbol.Clock }
+                );
+        }
+    }
 }
